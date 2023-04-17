3761ee5554bcfc1f60234b97ecab923ea1cb4824
yyeongseok <yeongseok0219@gmail.com>
2023-04-17 17:16:58 +0900

    [ADD]user entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from 'common/exception/http-exception.filter';
 import { AppModule } from './app.module';
+//import * as expressBasicAuth from 'expressBasicAuth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  //Swagger 설정
+  // app.use(
+  //   ['/docs', '/docs-json'],
+  //   expressBasicAuth({
+  //     challenge: true,
+  //     users: {
+  //       [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+  //     },
+  //   }),
+  // );
+
+  const config = new DocumentBuilder()
+    .setTitle('Noldiary')
+    .setDescription('The Noldiary API description')
+    .setVersion('1.0.0')
+    .addTag('diary')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('Noldiary', app, document);
+
+  app.enableCors({
+    origin: true,
+    credentials: true, //배포할때 false
+  });
+
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
