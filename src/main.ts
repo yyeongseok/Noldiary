94e07f377867b1d96364e0fe0c3c0538e14ee924
yyeongseok <yeongseok0219@gmail.com>
2023-04-18 15:16:52 +0900

    [Modify]mysql to mongoDB

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from 'common/exception/http-exception.filter';
+import { HttpExceptionFilter } from 'src/common/exception/http-exception.filter';
 import { AppModule } from './app.module';
-//import * as expressBasicAuth from 'expressBasicAuth';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
   //Swagger 설정
-  // app.use(
-  //   ['/docs', '/docs-json'],
-  //   expressBasicAuth({
-  //     challenge: true,
-  //     users: {
-  //       [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
-  //     },
-  //   }),
-  // );
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Noldiary')
